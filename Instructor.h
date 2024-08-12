1a86f4b7ceba6a9fdcedb7e32d6b14ca8aaa785b
theodoropoulosk <129194924+theodoropoulosk@users.noreply.github.com>
2024-08-11 23:25:15 -0400

    Add files via upload

---
+++ Instructor.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "User.h"
+#include "sqlite3.h"
+
+class Instructor :
+    public User
+{
+    // attribute
+    int hireyear;
+    string title, department;
+
+public:
+    // constructor
+    Instructor();
+    Instructor(string first, string last, string em, string in_title, string dept, int id, int year);
+
+    // method
+    void print_schedule();
+    void print_class_list();
+    void print_info();
+
+    // destructor
+    ~Instructor();
+};
---
+++ Student.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "User.h"
+
+class Student :
+    public User
+{
+    // attribute
+    int gradyear;
+    string major;
+
+public:
+    // constructor
+    Student();
+    Student(string first, string last, string em, string in_major, int id, int year);
+
+    // method
+    void add_drop_courses();
+    void print_schedule();
+    void print_info();
+
+    // destructor
+    ~Student();
+};
